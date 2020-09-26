db265d5d2c6ae8faace1b27fd167e09aa7c9d5dc
TreatTrick <1154174767@qq.com>
2020-09-26 15:27:52 +0800

    Initial Commit

---
+++ KittyWidgetV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KittyWidgetV2
+//
+//  Created by SORA on 2020/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KittyWidgetV2/KittyWidgetV2App.swift
@@ -0,0 +1,17 @@
+//
+//  KittyWidgetV2App.swift
+//  KittyWidgetV2
+//
+//  Created by SORA on 2020/9/26.
+//
+
+import SwiftUI
+
+@main
+struct KittyWidgetV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

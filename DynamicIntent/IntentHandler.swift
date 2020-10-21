ba3f4cff2e1f0d2591dc1175a6d1dd41c6d8e7b7
TreatTrick <1154174767@qq.com>
2020-10-22 00:50:39 +0800

    233

---
+++ DynamicIntent/IntentHandler.swift
@@ -0,0 +1,32 @@
+//
+//  IntentHandler.swift
+//  DynamicIntent
+//
+//  Created by SORA on 2020/10/21.
+//
+
+import Intents
+import SwiftUI
+
+class IntentHandler: INExtension, ConfigurationIntentHandling {
+    @EnvironmentObject var myData: MyData
+    func provideParameterOptionsCollection(for intent: ConfigurationIntent, with completion: @escaping (INObjectCollection<Widgets>?, Error?) -> Void) {
+        let widgets: [Widgets] = self.myData.dataStream.map{
+            let widget = Widgets(identifier: $0.id.uuidString, display: $0.name)
+            return widget
+        }
+        
+        let collection = INObjectCollection(items: widgets)
+        
+        completion(collection, nil)
+    }
+
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+    
+}
--- kittyWidgetExtension/kittyWidgetExtension.swift
+++ kittyWidgetExtension/kittyWidgetExtension.swift
@@ -10,41 +10,53 @@ import SwiftUI
 import Intents
 
 struct Provider: IntentTimelineProvider {
+    @EnvironmentObject var myData: MyData
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+        SimpleEntry(date: Date(), configuration: ConfigurationIntent(), basicData: BasicData(background: UIImage(named:"img1")!, display: .date, kitty: UIImage(named:"kitty1")!, name: "widget 1"))
     }
 
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        let entry = SimpleEntry(date: Date(), configuration: configuration, basicData: BasicData(background: UIImage(named:"img1")!, display: .date, kitty: UIImage(named:"kitty1")!, name: "widget 1"))
         completion(entry)
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let selectedWidget = selectWidget(for: configuration)
         let currentDate = Date()
+        var entries: [SimpleEntry] = []
+        let oneMinute: TimeInterval = 60
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            let entry = SimpleEntry(date: entryDate, configuration: configuration, basicData: selectedWidget)
             entries.append(entry)
         }
 
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
+    
+    func selectWidget(for configuration: ConfigurationIntent) -> BasicData{
+        if let idString = configuration.parameter?.identifier{
+            let id = UUID(uuidString: idString)!
+            return self.myData.dataStream.first(where: { $0.id == id })!
+        }
+        
+    }
 }
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
     let configuration: ConfigurationIntent
-    let basicData: BasicData = BasicData(background: UIImage(named:"img1")!, display: .date, kitty: UIImage(named:"kitty1")!, name: "widget 1")
+    let basicData: BasicData
 }
 
 struct kittyWidgetExtensionEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
+        Text("23333")
         SmallWidgetView2(basicData: entry.basicData, isKitty: entry.basicData.isKitty, isWord: entry.basicData.isWord, isBlur: entry.basicData.isBlur, isAllBlur: entry.basicData.isAllBlur, is24Hour: true, font: entry.basicData.font)
             .widgetURL(URL(string: "OpenedApp://")!)
     }
@@ -60,12 +72,13 @@ struct kittyWidgetExtension: Widget {
         }
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
+        .supportedFamilies([.systemSmall])
     }
 }
 
-struct kittyWidgetExtension_Previews: PreviewProvider {
-    static var previews: some View {
-        kittyWidgetExtensionEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
-    }
-}
+//struct kittyWidgetExtension_Previews: PreviewProvider {
+//    static var previews: some View {
+//        kittyWidgetExtensionEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent(), basicData: <#BasicData#>))
+//            .previewContext(WidgetPreviewContext(family: .systemSmall))
+//    }
+//}
